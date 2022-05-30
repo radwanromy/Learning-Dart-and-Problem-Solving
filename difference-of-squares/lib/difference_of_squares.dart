131ebf753efcbbe4e5c9ff15849534eda9686db8
A . S . M . RADWAN <50805604+radwanromy@users.noreply.github.com>
2022-05-30 15:24:12 +0600

    Commit Dart 07

--- difference-of-squares/lib/difference_of_squares.dart
+++ difference-of-squares/lib/difference_of_squares.dart
@@ -1,3 +1,36 @@
+import 'dart:math';
+
 class DifferenceOfSquares {
-  // Put your code here
+  List<int> makeSquare(int number) {
+    List<int> square = [];
+
+    for (int i = 1; i <= number; i++) square.add(i);
+
+    return square;
+  }
+  int squareOfSum(int number) {
+    num squareSum;
+    List<int> square = makeSquare(number);
+
+    squareSum = square.fold(0, (previousValue, element) {
+      return pow(element, 2);
+    });
+
+    return squareSum.toInt();
+  }
+  int sumOfSquares(int number) {
+    num sumSquare;
+    List<int> square = makeSquare(number);
+    sumSquare = pow(
+        square.fold(0, (previousValue, element) {
+          return previousValue + element;
+        }),
+        2);
+    return sumSquare.toInt();
+  }
+  int differenceOfSquares(int number) {
+    int squareSum = squareOfSum(number);
+    int sumSquare = sumOfSquares(number);
+    return squareSum - sumSquare;
+  }
 }
